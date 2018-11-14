8447de231bf2d2ccee31f98275be670d7452e850
andreivercosa <aluno@MacBook-Pro-de-Aluno.local>
2018-11-13 20:42:08 -0400

    lista de objetos

---
+++ lista_objeto/Carro.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace lista_objeto
+{
+    public class Carro
+    {
+        public string nome { get; set; }
+        public string anolancamento { get; set; }
+        public Carro()
+        {
+        }
+    }
+}
---
+++ lista_objeto/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lista_objeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Carro> listaCarro = new List<Carro>();
+
+            Carro carro1 = new Carro();
+            Carro carro2 = new Carro();
+            Carro carro3 = new Carro();
+
+            carro1.nome = "Gol";
+            carro1.anolancamento = "2008";
+            listaCarro.Add(carro1);
+
+            carro1.nome = "Palio";
+            carro1.anolancamento = "2002";
+            listaCarro.Add(carro2);
+
+            carro1.nome = "Fusca";
+            carro1.anolancamento = "1942";
+            listaCarro.Add(carro3);
+
+
+            foreach(var elemento in listaCarro){
+                Console.WriteLine("Nome: {0}\nAno de lancamento: {1}",elemento.nome,elemento.anolancamento);
+            }
+        }
+    }
+}
